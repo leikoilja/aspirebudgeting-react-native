438837c3200e425b51e13edb0da7682f465e6f41
Ilja Leiko <leikoilja@gmail.com>
2022-03-13 12:28:50 +0100

    Add eslint and husky precommits

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
+/*
+ eslint-disable @typescript-eslint/no-unsafe-assignment
+ */
+import React from "react";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 
-import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 
 import { store, persistor } from "@state/store";
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
   if (!isLoadingComplete) {
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,7 +1,12 @@
+/*
+ eslint-disable
+ @typescript-eslint/no-unsafe-argument,
+ @typescript-eslint/no-unsafe-assignment,
+ @typescript-eslint/no-unsafe-call,
+ */
 import thunk from "redux-thunk";
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
-import hardSet from "redux-persist/lib/stateReconciler/hardSet";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import userReducer from "./reducers/UserReducer";
@@ -16,13 +21,13 @@ const reducers = combineReducers({
   user: userReducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, reducers as any);
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   persistedReducer,
-  composeEnhancer(applyMiddleware(thunk)),
+  composeEnhancer(applyMiddleware(thunk))
 );
 /* eslint-enable */
 
