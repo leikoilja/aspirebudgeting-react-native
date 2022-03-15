21c5ec1d95ea972bae94d59d3b560b8310f25b15
Ilja Leiko <leikoilja@gmail.com>
2022-03-15 19:28:33 +0100

    Add react-native-elements UI library

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -8,6 +8,7 @@
 import React from "react";
 import { Text, View } from "react-native";
 import { useSelector } from "react-redux";
+import { Button } from "react-native-elements";
 import styles from "./HomeScreen.modules.css";
 
 const HomeScreen = () => {
@@ -17,6 +18,12 @@ const HomeScreen = () => {
     <View style={styles.container}>
       <Text style={styles.title}>This is styled using css</Text>
       <Text>Hey, {user.firstName ? user.firstName : "stranger"}</Text>
+      <Button
+        title="Green Button"
+        buttonStyle={styles.button}
+        containerStyle={styles.button_container}
+        titleStyle={styles.button_title}
+      />
     </View>
   );
 };
