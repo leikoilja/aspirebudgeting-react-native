fef860bdaa98ace8308744e437f9171015e8baef
Ilja Leiko <leikoilja@users.noreply.github.com>
2022-03-15 19:01:49 +0100

    Add CSS modules support + reset userProfile reducer (#6)

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,17 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     "plugins": [
       "inline-dotenv",
+      "react-native-classname-to-style",
+      ["react-native-platform-specific-extensions", { extensions: ["css"] }],
       [
         "module-resolver",
         {
           "root": ["./src"],
           "alias": {
+            "@auth": "./src/auth",
             "@actions": "./src/state/actions",
             "@const": "./src/const.ts",
             "@libs": "./src/libs",
