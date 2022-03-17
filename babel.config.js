e113bd4198e84b201802ff2af19994b0bd1c355d
Ilja Leiko <leikoilja@gmail.com>
2022-03-17 13:17:35 +0100

    Adding shared theme & custom loader component

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,7 @@ module.exports = function(api) {
             "@auth": "./src/auth",
             "@actions": "./src/state/actions",
             "@const": "./src/const.ts",
+            "@components": "./src/components",
             "@libs": "./src/libs",
             "@state": "./src/state",
             "@types": "./src/types.tsx",
